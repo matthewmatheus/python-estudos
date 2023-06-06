22ea9f6230ce6c4429d96fb919345c826772cac5
Matt <mattmoura.jvm@gmail.com>
2023-06-06 13:43:50 -0300

    estudos python

--- json.py
+++ json.py
@@ -2,9 +2,21 @@ import json
 from builtins import print
 
 
-arquivo = open("C:\\Users\\Usuário\Documents\\dicionario.json","r", encoding="UTF-8")
-conteudo = arquivo.read()
-arquivo.close()
-agenda = json.loads(conteudo)
+with open("C:\\Users\\Usuário\Documents\\dicionario.json","w", encoding="UTF-8") as arquivo:
+    arquivo.write("Teste testando")
 
-print(type(agenda))
+    print("Arquivo salvo")
+    # print(arquivo.read)
+
+
+
+
+#
+# arquivo = open("C:\\Users\\Usuário\Documents\\dicionario.json","r", encoding="UTF-8")
+# conteudo = arquivo.read()
+# arquivo.close()
+# agenda = json.loads(conteudo)
+#
+# print(type(agenda))
+
+
